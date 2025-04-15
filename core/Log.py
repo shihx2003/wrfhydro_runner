fb8ca73b8eb14e3b4b9e4e90ce11a7b5ca595286
shihx2003 <shihx2003@outlook.com>
2025-04-15 19:33:46 +0800

    Improve : use Log.py

--- core/Log.py
+++ core/Log.py
@@ -16,6 +16,47 @@ class Log:
     def __init__(self, loggername=None, level=logging.DEBUG):
         self.name = loggername if loggername else datetime.now().strftime('%Y-%m-%d-%H:%M')
         self.log = logging.getLogger(loggername)
+<<<<<<< HEAD
+=======
+        self.log.setLevel(level)
+
+        formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+
+        if not os.path.exists('./logs'):
+            os.makedirs('./logs')
+        
+        file_handler = logging.FileHandler(f'./logs/{loggername}.log')
+        file_handler.setLevel(level)
+        file_handler.setFormatter(formatter)
+
+        self.log.addHandler(file_handler)
+    
+    def get_log(self):
+        return self.log
+    
+
+if __name__ == '__main__':
+    logger = Log('test').get_log()
+# -*- encoding: utf-8 -*-
+'''
+@File    :   Log.py
+@Create  :   2025-04-15 18:56:14
+@Author  :   shihx2003
+@Version :   1.0
+@Contact :   shihx2003@outlook.com
+'''
+
+# here put the import lib
+import os
+import logging
+from datetime import datetime
+
+class Log:
+    def __init__(self, name, logfile=None, level=logging.DEBUG):
+
+        self.name = name 
+        self.log = logging.getLogger(name)
+>>>>>>> 7b3a77d (Improve : use Log.py)
         self.log.setLevel(level)
 
         formatter = logging.Formatter('%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
