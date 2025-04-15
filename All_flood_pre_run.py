33562afc56c82654a4154de21933032c96b3aaf7
shihx2003 <shihx2003@outlook.com>
2025-04-15 22:27:23 +0800

    fix : log error

--- All_flood_pre_run.py
+++ All_flood_pre_run.py
@@ -10,26 +10,26 @@
 # here put the import lib
 import os
 import yaml
+import logging
+from core import SimulationInfo, batch_instantiate, schedule_and_track_jobs
+from core import Log
 
-from core import Log, SimulationInfo, batch_instantiate, schedule_and_track_jobs
-logger = Log('All_flood_test').get_log()
-logger.debug("Main script started.")
-logger.debug("This is a debug message")
-logger.info("This is an info message")
-logger.warning("This is a warning message")
-logger.error("This is an error message")
-logger.critical("This is a critical message")
+log_dir = r'./logs'
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
 
-event_no = [
-    '20160718',
-    '20190804',
-    '20120721',
-    '20200717',
-    '20130811',
-    '20200801',
-    '20200824',
-    '20120621'
-]
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(os.path.join(log_dir, 'mian.log')),
+        logging.StreamHandler()
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+event_no = ['20160718', '20190804', '20120721','20200717','20130811','20200801','20200824','20120621']
 sim_info = {
     'obj': 'All_flood_test',
     'ROOT_DIR': './',
@@ -55,6 +55,8 @@ for event in event_no:
     run_jobs[job_info['job_id']] = job_info
 
 run_jobs_path = os.path.join(global_info.ROOT_DIR, 'job/All_test.yaml')
+if not os.path.exists(run_jobs_path):
+    os.makedirs(os.path.dirname(run_jobs_path), exist_ok=True)
 with open(run_jobs_path, 'w') as f:
     yaml.dump(run_jobs, f, default_flow_style=False)
 
